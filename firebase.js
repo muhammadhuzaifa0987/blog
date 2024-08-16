d5f66fe6adca41d8c23b5081d43d833c581e6b13
muhammadhuzaifa0987 <164562502+muhammadhuzaifa0987@users.noreply.github.com>
2024-08-16 14:00:02 -0700

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,52 @@
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
+import {getFirestore,collection, addDoc, getDocs,doc,updateDoc,
+  deleteDoc, setDoc,  getDoc,
+} from "https://www.gstatic.com/firebasejs/10.12.4/firebase-firestore.js";
+
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
+
+import {
+    getStorage,
+    ref,
+    uploadBytesResumable,
+    getDownloadURL,
+  } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-storage.js";
+
+//configure firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyDDI02mLG4frGRerIodYR2B7zJLX-RKQVI",
+    authDomain: "first-firebase-project-d07af.firebaseapp.com",
+    projectId: "first-firebase-project-d07af",
+    storageBucket: "first-firebase-project-d07af.appspot.com",
+    messagingSenderId: "715305896838",
+    appId: "1:715305896838:web:c32f2bf6271f9cfb043a07"
+  };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// Initialize firestore
+const db = getFirestore(app);
+// Initialize Auth
+const auth = getAuth();
+// Initialize STORAGE
+const storage= getStorage();
+
+export {
+  app,db,collection,addDoc,getDocs,doc,
+  updateDoc,
+  deleteDoc,
+  auth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  setDoc,
+  getDoc,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+};
